4b9d268fa42148fcbbf4237c254e576a815b06a8
KRONO\Carlos <adan_alda@hotmail.com>
2021-07-15 13:06:26 +0200

    mocker funcionando

--- refresh.py
+++ refresh.py
@@ -1,8 +1,27 @@
+import json
+
 while True:
     # TODO peticion a BBDD
 
     response = None
 
+    # TODO mockear el response
+
+    data = None
+    mensajes =[]
+    with open("refreshMocker.json") as jsonFile:
+        data = json.load(jsonFile)
+    
+    for element in data["mensajes"]:
+        mensaje = [
+            element["user"],
+            element["timestamp"],
+            element["txt"]
+            ]
+        mensajes.append(mensaje)
+
+    # print(mensajes)
+    
     # TODO generar JSON desde el response de BDD
 
     # TODO enviar JSON a FRONT
